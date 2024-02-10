01b15e2fff7a2295985f7eec3a91ab1cdb16d41e
codezoneo <anuragtri83@gmail.com>
2024-02-10 22:11:03 +0530

    newCommitChange

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,6 +1,6 @@
 // AuthContext.js
 
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 
 const AuthContext = createContext();
 
@@ -9,15 +9,25 @@ export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
 
+  // Update token in state and localStorage when logging in
   const login = (newToken) => {
     setToken(newToken);
     localStorage.setItem("token", newToken);
   };
 
+  // Clear token from state and localStorage when logging out
   const logout = () => {
-    setToken(null); // Set token to null
-    localStorage.removeItem("token"); // Remove token from localStorage
+    setToken(null);
+    localStorage.removeItem("token");
   };
+
+  // Load token from localStorage on component mount (i.e., when app starts)
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ token, login, logout }}>
